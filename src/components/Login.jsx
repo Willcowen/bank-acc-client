79575624578d9b06b60ffd332d243753a329d388
Will <willcowendrums@gmail.com>
2022-10-05 12:18:33 +0100

    progress on login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import "./Login.css";
 
 function Login() {
   return (
     <form className="login">
-      <label for="email">Email</label>
-      <input type="text" id="formfname" name="fname" value="John" />
-      <label for="lname">Password</label>
-      <input type="text" id="lname" name="lname" value="Doe" />
-      <input type="submit" value="Submit" />
+      <h1>Transaction App</h1>
+      <div className="login--email">
+        <label for="email">Email</label>
+        <input type="text" id="login--email--input" name="email" />
+      </div>
+      <div className="login--password">
+        <label for="lname">Password</label>
+        <input type="password" id="login--password--input" name="password" />
+      </div>
+      <button type="submit" id="login--button">
+        Login
+      </button>
+      <p>Need an account? Sign up here</p>
     </form>
   );
 }
